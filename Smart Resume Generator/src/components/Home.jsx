5b11cf80bba4bfdfb41e4c865f7ce523d556f2dd
Divyansh Kumar <divyansh14kr@gmail.com>
2024-10-14 01:17:51 +0530

    changes made

--- Smart Resume Generator/src/components/Home.jsx
+++ Smart Resume Generator/src/components/Home.jsx
@@ -8,8 +8,26 @@ function Home(){
         <h1>Resume Builder with AI</h1>
         <p>Create a job winning resume with the help of AI in senconds</p>
         <form action="">
+            
+            <label htmlFor="fullname">Enter your full name</label>
+            <input name="fullname" 
+            type="text" 
+            required="true" 
+            id="fullname" 
+            value={fullname} onChange={(e )=>{setFullName(e.target.value)}}/>
+
+
+            <label htmlFor="currentposition">Current Posotion</label>
+            <input name="currentPosition" 
+                type="text"
+                required="true"
+                id="currentPosition" 
+                value={fullname} 
+                onChange={(e )=>{setFullName(e.target.value)}}/>
+
             <label htmlFor="fullname">Enter your full name</label>
             <input name="fullname" type="text" required="true" id="fullname" value={fullname} onChange={(e )=>{setFullName(e.target.value)}}/>
+
         </form>
     </div>
 }
