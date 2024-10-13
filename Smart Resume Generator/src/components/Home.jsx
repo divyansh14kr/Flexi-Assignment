1b8a822bb45c17789e838ce70ffa5bf27ab49543
Divyansh Kumar <divyansh14kr@gmail.com>
2024-10-14 01:18:00 +0530

    changes made

--- Smart Resume Generator/src/components/Home.jsx
+++ Smart Resume Generator/src/components/Home.jsx
@@ -4,11 +4,12 @@ import Loading from "./Loading";
 function Home(){
 
     const [fullname,setFullName] = useState("");
+    const [currentPostiion, setCurrenPostion] = useState("");
     return <div>
         <h1>Resume Builder with AI</h1>
         <p>Create a job winning resume with the help of AI in senconds</p>
         <form action="">
-            
+
             <label htmlFor="fullname">Enter your full name</label>
             <input name="fullname" 
             type="text" 
@@ -21,9 +22,8 @@ function Home(){
             <input name="currentPosition" 
                 type="text"
                 required="true"
-                id="currentPosition" 
-                value={fullname} 
-                onChange={(e )=>{setFullName(e.target.value)}}/>
+                value={currentPostiion} 
+                onChange={(e )=>{setCurrenPostion(e.target.value)}}/>
 
             <label htmlFor="fullname">Enter your full name</label>
             <input name="fullname" type="text" required="true" id="fullname" value={fullname} onChange={(e )=>{setFullName(e.target.value)}}/>
