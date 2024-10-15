6797e9c374fabf12f8f7362bd16894a810b94d83
Divyansh Kumar <divyansh14kr@gmail.com>
2024-10-15 11:11:41 +0530

    changes made to App.jsx

--- Smart Resume Generator/src/App.jsx
+++ Smart Resume Generator/src/App.jsx
@@ -5,6 +5,7 @@ import Loading from "../src/components/Loading";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Form from '../src/components/Form';
 import { Suspense } from 'react';
+import './App.css'
 
 function App() {
   const [count, setCount] = useState(0);
--- Smart Resume Generator/src/components/Home.jsx
+++ Smart Resume Generator/src/components/Home.jsx
@@ -7,37 +7,50 @@ function Home(){
     const [fullname,setFullName] = useState("");
     const [currentPostiion, setCurrenPostion] = useState("");
     const [currentLength, setCurrentLength] = useState(1); 
-    const [ currentStack , setCurrentStack] = useState({})
+    const [ currentStack , setCurrentStack] = useState("")
     const [image, setImage] = useState(null)
+
+    const handelFormSubmit= (e)=>{
+        e.preventDefault();
+        console.log({
+            fullname,
+            currentPostiion,
+            currentLength,
+            currentStack,
+            imagex
+        })
+    }
+
     return <div>
         <h1>Resume Builder with AI</h1>
-        <p>Create a job winning resume with the help of AI in senconds</p>
-        <form action="">
+        <p>Create a job winning resume with the help of AI in seconds</p>
+        <form onSubmit={handelFormSubmit} method="POST">
 
             <label htmlFor="fullname">Enter your full name</label>
             <input name="fullname" 
             type="text" 
-            required="true" 
+            required
             id="fullname" 
             value={fullname} onChange={(e )=>{setFullName(e.target.value)}}/>
 
-            <div className="nestedContainr">
+            <div className="nestedContainer">
 
                 <div>
-                    <label htmlFor="currentPosition">Current Posotion</label>
+                    <label htmlFor="currentPosition">Current Position</label>
                     <input name="currentPosition" 
                         type="text"
-                        required="true"
+                        required
+                        className="currentInput"
                         value={currentPostiion} 
                         onChange={(e )=>{setCurrenPostion(e.target.value)}}/>
 
 
                 </div>
                 <div>
-                    <label htmlFor="currentLength">Duration of current position (in years)</label>
+                    <label htmlFor="currentLength">Duration of current position (in years) </label>
                     <input name="currentLength" 
                         type="text"
-                        required="true"
+                        required
                         value={currentLength} 
                         onChange={(e )=>{setCurrentLength(e.target.value)}}/>
 
@@ -46,18 +59,19 @@ function Home(){
                     <label htmlFor="currentStack">Current Tech Stack</label>
                     <input name="currentStack" 
                         type="text"
-                        required="true"
-                        value={currentStack} 
+                        required
+                        value={currentStack}
+                        className="currentInput" 
                         onChange={(e )=>{setCurrentStack(e.target.value)}}/>
                 </div>
             </div>
         
             <div>
-                    <label htmlFor="photo">Upload your image</label>
+                    <label htmlFor="photo">Upload your image </label>
                     <input name="photo" 
                         type="file"
                         id= "photo"
-                        required="true"
+                        required
                         accept="image/x-png,image/jpeg"
                         onChange={(e )=>{setImage(e.target.files[0])}}/>
                 </div>
