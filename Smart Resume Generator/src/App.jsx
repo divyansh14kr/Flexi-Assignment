43344361db33e6f773187664bfcdc8beb71ed343
Trisha Boda <141979558+trishaboda@users.noreply.github.com>
2024-10-17 21:56:07 +0530

    updated login, signup, form

--- Smart Resume Generator/src/App.jsx
+++ Smart Resume Generator/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
-import Home from "../src/components/Home";
+//import Home from "../src/components/Home";
 import Resume from "../src/components/Resume";
 import Loading from "../src/components/Loading";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Form from '../src/components/Form';
 import { Suspense } from 'react';
 import './App.css'
+import LoginPage from '../src/components/login';
+import Signup from '../src/components/signup';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -15,9 +17,11 @@ function App() {
       <div className='APP'>
         <Suspense fallback={<Loading />}>
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={<LoginPage />} />
             <Route path="/resume" element={<Resume />} />
             <Route path="/form" element={<Form />} />
+            <Route path="/signup" element={<Signup />} />
+
           </Routes>
         </Suspense>
       </div>
