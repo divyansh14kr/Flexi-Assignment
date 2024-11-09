703c48a698f723f87b251c85af8e6371445ee29a
Divyansh Kumar <divyansh14kr@gmail.com>
2024-11-09 15:02:45 +0530

    changes

--- Smart Resume Generator/src/redux/store.js
+++ Smart Resume Generator/src/redux/store.js
@@ -1,6 +1,8 @@
-import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
+import { combineReducers } from 'redux';
+
 import profileReducer from './profileSlice';
 import educationReducer from './educationSlice';
 import projectReducer from './projectSlice';
@@ -8,11 +10,13 @@ import experienceReducer from './experienceSlice';
 import extraDetailsReducer from './extraDetailsSlice';
 import userReducer from './userSlice';
 
+// Persist configuration
 const persistConfig = {
   key: 'root',
   storage,
 };
 
+// Combine reducers
 const rootReducer = combineReducers({
   profileDetails: profileReducer,
   educationDetails: educationReducer,
@@ -22,10 +26,17 @@ const rootReducer = combineReducers({
   user: userReducer,
 });
 
+// Persisted reducer
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
   reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ['persist/PERSIST'],  // Ignore persist actions from serializability check
+      },
+    }),
 });
 
 export const persistor = persistStore(store);
