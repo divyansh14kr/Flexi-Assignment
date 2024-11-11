24aaa7aa90eb18da12097c53d1784f82b02f3ff2
Divyansh Kumar <divyansh14kr@gmail.com>
2024-11-11 23:44:42 +0530

    api

--- Smart Resume Generator/src/api.js
+++ Smart Resume Generator/src/api.js
@@ -1,3 +1,4 @@
-export const BASE_URL = "https://flexi-assignment.onrender.com/api";
+// export const BASE_URL = "http://localhost:3000/api";
+export const BASE_URL= "https://flexi-assignment.onrender.com/api"
 // export const BASE_URL = "https://resume-builder-zkad.onrender.com/api"
 //export const BASE_URL = "https://resume-builder-mern-one.vercel.app/api"
